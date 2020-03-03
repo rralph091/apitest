dc2693aaf74c8a35a27f3db328d3ff91472fe711
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-03-03 10:54:31 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -1,8 +1,26 @@
+<?php
+	//category
+	$json = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
+	$data = json_decode($json,true);
+	$category = $data['records'];
+?>
+<center>
 <form action="pro_create.php" method="POST">
+<h1> Create Product </h1>
 <input type="text" name="name" placeholder="name"/>
 <input type="text" name="description" placeholder="description"/>
 <input type="text" name="price" placeholder="price"/>
-<input type="text" name="category" placeholder="category"/>
+<select name="category">
+<option value="">--Category--</option>
+	<?php
+      foreach($category as $cview){
+    ?>
+		<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
+    <?php
+      }
+    ?>
+	</select>
 <input type="submit" name="submit" value="submit"/>
 
 </form>
+</center>
