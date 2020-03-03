95f254bddffd7c9b91e9dbbebca7216fa28fdaa9
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-03-03 10:32:26 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+$navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET['navigation']: '';
+?>
+<html> 
+    <head>
+      <title>Endterm Activity #1</title>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+      <?php include ('sidebar.php'); ?>
+
+
+    
+    </body>
+</html>
