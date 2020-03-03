c51a7a5b55ceb2bcb2bcf1f6eac8b84d9eddbf35
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-03-03 10:33:26 +0800

    Create body.php

---
+++ body.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<center>
+    <h1>BODY</h1>
+        This Is Where The Intro Is placed, It must Be Interesting to catch The users Eye 
+        </center>
+      
+</body>
+</html>
