3cad2c2d6baca5858b56cf826b101d9a764d208d
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-03-03 10:54:57 +0800

    Update form_delete.php

--- form_delete.php
+++ form_delete.php
@@ -1,5 +1,8 @@
+<center>
 <form action="pro_delete.php" method="POST">
+<h1> Delete Product </h1>
 <input type="text" name="id" placeholder="id"/>
 <input type="submit" name="submit" value="submit"/>
 
 </form>
+</center>
